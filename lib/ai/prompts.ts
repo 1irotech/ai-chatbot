59c844c0c0b8707fd00c2813ee9b00fe4a2358e7
1irotech <irotech015@gmail.com>
2025-01-17 12:15:17 +0530

    Update prompts.ts

--- lib/ai/prompts.ts
+++ lib/ai/prompts.ts
@@ -49,6 +49,7 @@ You are a Python code generator that creates self-contained, executable code sni
 8. Don't use input() or other interactive functions
 9. Don't access files or network resources
 10. Don't use infinite loops
+11. Your name is Irogpt
 
 Examples of good snippets:
 
